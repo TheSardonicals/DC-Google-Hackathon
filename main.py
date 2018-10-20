21f7535276cb2814db219ff57c392745fe850e9d
Dexsidius <dashawnlarry@gmail.com>
2018-10-20 19:49:17 -0400

    derp

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ os.environ["PYSDL2_DLL_PATH"] = os.path.dirname(os.path.abspath(__file__))
 from sdl2 import *
 from sdl2.sdlmixer import *
 
-
-
 class Music:
     def __init__(self):
         Mix_Init(MIX_INIT_MP3)
